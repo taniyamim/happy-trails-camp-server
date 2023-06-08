f861ed63ac552a90ce4e6a0c0a2ebf9d02a736fe
taniyamim <[jannatulhafsa500@gmail.com]>
2023-06-09 00:13:19 +0600

    delete added

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ctecrti.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -61,6 +61,13 @@ async function run() {
            
           
           });
+          app.delete('/selectedClasses/:id',  async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const query = { _id: new ObjectId(id) }
+            const result = await selectedCollection.deleteOne(query);
+            res.send(result);
+          })
 
 
 
